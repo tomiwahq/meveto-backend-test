478b0dd7831d4c00b6600370cdb08fb9d14ce158
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2022-05-06 00:00:25 +0100

    Creates customers migration

---
+++ database/migrations/2022_05_05_131044_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('title', 50);
+            $table->string('email', 100);
+            $table->string('gender', 20);
+            $table->string('company', 100);
+            $table->string('city', 100);
+            $table->double('longitude')->nullable();
+            $table->double('latitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
