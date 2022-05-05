67b2ce6d07b1d4d1a4bdf6f43d5f6ab92b4a60ed
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2022-05-05 23:58:42 +0100

    Creates Customers model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,12 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'firstname',
-        'lastname',
+        'name',
         'email',
         'password',
-        'longitude',
-        'latitude',
     ];
 
     /**
