85b191d753c3287d7c0b8b7d9a71278356ee0a74
Tomiwa Ibiwoye <tomiwa@teraboxx.com>
2022-05-06 03:32:55 +0100

    Makes all fields nullable

--- database/migrations/2022_05_05_131044_create_customers_table.php
+++ database/migrations/2022_05_05_131044_create_customers_table.php
@@ -14,13 +14,13 @@ return new class extends Migration {
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname', 50);
-            $table->string('lastname', 50);
-            $table->string('title', 50);
-            $table->string('email', 100);
-            $table->string('gender', 20);
-            $table->string('company', 100);
-            $table->string('city', 100);
+            $table->string('firstname', 50)->nullable();
+            $table->string('lastname', 50)->nullable();
+            $table->string('title', 50)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('gender', 20)->nullable();
+            $table->string('company', 100)->nullable();
+            $table->string('city', 100)->nullable();
             $table->double('longitude')->nullable();
             $table->double('latitude')->nullable();
             $table->timestamps();
